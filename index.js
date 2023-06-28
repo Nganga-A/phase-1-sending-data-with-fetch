f7c47243c690a272f77f23d308a003736da3f09b
Abed Nganga <ngangaabed@gmail.com>
2023-06-28 12:12:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    const data = {
+        name: userName,
+        email: userEmail,
+    }
+
+    const postObject = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type":"application/json",
+            accept:"application/json"
+        } 
+    }
+
+     return fetch('http://localhost:3000/users', postObject)  //return returns the promise to error handling incase of error
+     .then (response => response.json())
+    .then(data => {
+        document.querySelector("body").textContent = data.id 
+    })
+    .catch((err) => {
+        document.querySelector("body").textContent = err.message;
+      });
+    
+  
+    }
